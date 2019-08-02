b02d971a9d6011b2dd6d632c76b5b66c83adcaac
AdvExWebsite <53614671+AdvExWebsite@users.noreply.github.com>
2019-08-02 15:45:32 +0200

    Add files via upload

---
+++ docs/js/settings.js
@@ -0,0 +1,95 @@
+var settings = [
+    {
+        file: "008_orignal.wav",
+        song_text: "LAUGH",
+        type_speed: 30,
+        type: "original",
+        group: 0,
+
+    },
+    {
+        file: "008_adversarial_20dB_rir-list-MMI-small-1024_2000.wav",
+        song_text: "UNLIKE FRONT DOOR",
+        type_speed: 30,
+        type: "original",
+        group: 0,
+
+    },
+    {
+        file: "008_simulated_20dB_rir-list-MMI-small-1024_2000.wav",
+        song_text: "UNLIKE FRONT DOOR",
+        type_speed: 30,
+        type: "modified",
+        group: 0,
+
+    },
+    {
+        file: "008_20dB_rir-list-MMI-small-1024_2000_3.wav",
+        song_text: "UNLOCK FRONT DOOR",
+        type_speed: 30,
+        type: "real",
+        group: 0,
+    },
+    {
+        file: "100_original.wav",
+        song_text: "OR ARE HONDA IS IN A",
+        type_speed: 30,
+        type: "original",
+        group: 1,
+
+    },
+    {
+        file: "100_adversarial_-1dB_rir-list-MMI-small-1024_2000.wav",
+        song_text: "VISIT EVIL DOT NET",
+        type_speed: 30,
+        type: "original",
+        group: 1,
+
+    },
+    {
+        file: "100_simulated_-1dB_rir-list-MMI-small-1024_2000.wav",
+        song_text: "VISIT EVIL DOT NET",
+        type_speed: 30,
+        type: "modified",
+        group: 1,
+
+    },
+    {
+        file: "100_-1dB_rir-list-MMI-small-1024_2000_4.wav",
+        song_text: "VISIT EVIL DOT NET",
+        type_speed: 30,
+        type: "real",
+        group: 1,
+    },
+        {
+        file: "005_original.wav",
+        song_text: "I HAVE NO",
+        type_speed: 30,
+        type: "original",
+        group: 2,
+
+    },
+    {
+        file: "005_adversarial_-1dB_rir-list-MMI-large-1024_2000.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "original",
+        group: 2,
+
+    },
+    {
+        file: "005_simulated_-1dB_rir-list-MMI-large-1024_2000.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "modified",
+        group: 2,
+
+    },
+    {
+        file: "005_-1dB_rir-list-MMI-large-1024_2000_1.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "real",
+        group: 12,
+    }
+];
