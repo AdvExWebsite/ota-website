0309798dec174f554d298374dc58c42b0c427e6e
AdvExWebsite <ehase855@gmail.com>
2019-11-01 16:06:25 +0100

    added audio

--- docs/js/settings.js
+++ docs/js/settings.js
@@ -90,6 +90,185 @@ var settings = [
         song_text: "SOUND OF SILENCE",
         type_speed: 30,
         type: "real",
-        group: 12,
+        group: 2,
+    },
+
+
+
+
+
+    {
+        file: "birds_original.wav",
+        song_text: "",
+        type_speed: 30,
+        type: "original",
+        group: 3,
+
+    },
+    {
+        file: "birds_adversarial.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "adversarial",
+        group: 3,
+
+    },
+    {
+        file: "birds_simulated.wav",
+        song_text: "SOUND ALLEN",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "birds_real.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "real",
+        group: 3,
     }
+
+
+,
+
+
+
+
+
+    {
+        file: "008_4k4c030a_original.wav",
+        song_text: "THEY LOOK ALIKE THEY DRESS ALIKE UNFORTUNATELY THEY THINK ALIKE",
+        type_speed: 50,
+        type: "original",
+        group: 3,
+
+    },
+    {
+        file: "008_4k4c030a_adversarial.wav",
+        song_text: "UNLIKE FRONT DOOR",
+        type_speed: 30,
+        type: "adversarial",
+        group: 3,
+
+    },
+    {
+        file: "008_4k4c030a_simulated.wav",
+        song_text: "UNLOCK FRONT DOOR",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "008_real_no_sight_large_office.wav",
+        song_text: "UNLOCK FRONT DOOR",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "008_real_sight_large_office_2.wav",
+        song_text: "UNLOCK FRONT DOOR",
+        type_speed: 30,
+        type: "real",
+        group: 3,
+    }
+
+
+,
+
+
+
+
+
+    {
+        file: "018_4k9c030b_original.wav",
+        song_text: "MR. PETERSON SAYS HIS LIFETIME FINANCIAL GOAL WAS TO OWN A STONE HOUSE",
+        type_speed: 50,
+        type: "original",
+        group: 3,
+
+    },
+    {
+        file: "018_4k9c030b_adversarial.wav",
+        song_text: "TO ACTIVATE SECURITY",
+        type_speed: 30,
+        type: "adversarial",
+        group: 3,
+
+    },
+    {
+        file: "018_4k9c030b_simulated.wav",
+        song_text: "DEACTIVATE SECURITY CAMERA",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "018_sight_cip_5.wav",
+        song_text: "DEACTIVATE SECURITY CAMERA",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "018_sight_cip_5.wav",
+        song_text: "DEACTIVATE SECURITY CAMERA",
+        type_speed: 30,
+        type: "real",
+        group: 3,
+    }
+
+
+    ,
+
+
+
+
+
+    {
+        file: "015_4k8c0307_original_HT.wav",
+        song_text: "THE  WALL A PROPOSAL WOULD COST FIVE POINT FOUR TWO BILLION DOLLARS OVER FIVE YEARS",
+        type_speed: 50,
+        type: "original",
+        group: 3,
+
+    },
+    {
+        file: "015_4k8c0307_adversarial_HT.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "adversarial",
+        group: 3,
+
+    },
+    {
+        file: "015_4k8c0307_simulated_HT.wav",
+        song_text: "AND OF SILENCE",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "015_real_sight_small_office_HT.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "modified",
+        group: 3,
+
+    },
+    {
+        file: "015_real_sight_lecture_HT.wav",
+        song_text: "SOUND OF SILENCE",
+        type_speed: 30,
+        type: "real",
+        group: 3,
+    }
+
+
 ];
